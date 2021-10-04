4dad445a06181be49fcd43e89e66960daa9c59cf
sse9173 <sse9173@gmail.com>
2021-10-04 15:40:21 +0900

    20211004 LeetCode

---
+++ Island_Perimeter.py
@@ -0,0 +1,16 @@
+# 20211004
+# LeetCode
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        row, col = len(grid), len(grid[0])
+        ret = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 1:
+                    if i == 0 or grid[i - 1][j] == 0: ret += 1
+                    if j == col - 1 or grid[i][j + 1] == 0: ret += 1
+                    if i == row - 1 or grid[i + 1][j] == 0: ret += 1
+                    if j == 0 or grid[i][j - 1] == 0: ret += 1
+        return ret
+
